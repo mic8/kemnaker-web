b20051f26ed8a623382f86d84874ed33dd3b9b3b
Michael Reynald <michaelreynald78@gmail.com>
2018-07-02 15:55:36 +0700

    Initial commit

---
+++ apps/backend/src/bootstrap/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'naker-root',
+    templateUrl: './app.component.html',
+})
+export class AppComponent {
+    public title: string = 'ubud';
+}
